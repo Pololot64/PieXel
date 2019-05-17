08cb3e5985c74aad27fa27ba6df574debeeacc4b
piecubed <piecubed@users.noreply.github.com>
2019-05-17 11:23:46 -0400

    Uploading files

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+pygame.init()
+size = width, height = 640, 640
+blue = 18, 171, 255
+screen = pygame.display.set_mode(size)
+screen.fill(blue)
+pygame.display.flip()
+for x in range(0, 640, 64):
+    for y in range(0, 640, 64):
+        if y/64 <= 5:
+            pass
+        elif y/64 == 6:
+            screen.blit(pygame.image.load('dirt.png'), (x, y))
+        else:
+            screen.blit(pygame.image.load('dirt2.png'), (x, y))
+        print('Generating image at ({}, {})'.format(x, y))
+char_pos = [0, 64*4]
+screen.blit(pygame.image.load('pie_character_64x128.png'), tuple(char_pos))
+pygame.display.flip()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        screen.blit(pygame.image.load('sky_char.png'), tuple(char_pos))
+        char_pos[0] -= 1
+        screen.blit(pygame.image.load('pie_character_64x128.png'), tuple(char_pos))
+        pygame.display.flip()
+    elif keys[pygame.K_d]:
+        screen.blit(pygame.image.load('sky_char.png'), tuple(char_pos))
+        char_pos[0] += 1
+        screen.blit(pygame.image.load('pie_character_64x128.png'), tuple(char_pos))
+        pygame.display.flip()
